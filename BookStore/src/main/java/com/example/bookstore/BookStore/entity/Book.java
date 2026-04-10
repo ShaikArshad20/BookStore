049ce2d262bfe222a5d764717614e5c2fb8a7fd3
Arshad <arshadshaik6060@gmail.com>
2026-04-10 18:25:45 +0530

    Pushed staged commits

--- BookStore/src/main/java/com/example/bookstore/BookStore/entity/Book.java
+++ BookStore/src/main/java/com/example/bookstore/BookStore/entity/Book.java
@@ -1,4 +1,46 @@
 package com.example.bookstore.BookStore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    private Long BookId;
+    private String BookName;
+    private String Author;
+    private double price;
+
+    public Long getBookId() {
+        return BookId;
+    }
+
+    public void setBookId(Long bookId) {
+        BookId = bookId;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public void setBookName(String bookName) {
+        BookName = bookName;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- BookStore/src/main/java/com/example/bookstore/BookStore/repository/BookRepository.java
+++ BookStore/src/main/java/com/example/bookstore/BookStore/repository/BookRepository.java
@@ -3,5 +3,5 @@ package com.example.bookstore.BookStore.repository;
 import com.example.bookstore.BookStore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
